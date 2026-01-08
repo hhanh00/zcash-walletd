4a74ce7d1aff531daa38dec14519286730754b33
Hanh <hanh425@gmail.com>
2026-01-08 21:38:00 +0800

    fix: notify_tx called before db_tx.commit

--- src/db.rs
+++ src/db.rs
@@ -7,9 +7,9 @@ use crate::{notify_tx, Client, Hash};
 use anyhow::Result;
 use sqlx::sqlite::{SqliteConnectOptions, SqliteRow};
 use sqlx::{Acquire, Row, SqliteConnection, SqlitePool};
+use std::collections::HashMap;
 use tokio::sync::Mutex;
 use tonic::Request;
-use std::collections::HashMap;
 use zcash_keys::address::UnifiedAddress;
 use zcash_keys::encoding::AddressCodec;
 use zcash_keys::keys::{UnifiedAddressRequest, UnifiedFullViewingKey};
@@ -319,11 +319,7 @@ impl Db {
         Ok(())
     }
 
-    pub async fn fetch_block_hash(
-        &self,
-        client: &mut Client,
-        height: u32,
-    ) -> Result<()> {
+    pub async fn fetch_block_hash(&self, client: &mut Client, height: u32) -> Result<()> {
         let mut connection = self.pool.acquire().await?;
         if sqlx::query("SELECT 1 FROM blocks WHERE height = ?1")
             .bind(height)
@@ -470,17 +466,22 @@ impl Db {
         let mut connection = self.pool.acquire().await?;
         let mut db_transaction = connection.begin().await?;
         let db_tx = db_transaction.acquire().await?;
+        let mut notify_txids = vec![];
 
         for event in events {
             match event {
                 ScanEvent::Received(received_note) => {
-                    let id_tx = self
+                    let (id_tx, is_new) = self
                         .create_tx_if_not_exists(
                             received_note.height,
                             received_note.txid.as_slice(),
                             db_tx,
                         )
                         .await?;
+                    if is_new {
+                        notify_txids.push(received_note.txid);
+                    }
+
                     let (account, sub_account) = match sqlx::query(
                         "SELECT a.account, a.sub_account FROM addresses a
                         JOIN receivers r ON a.id_address = r.id_address
@@ -606,6 +607,12 @@ impl Db {
         }
         db_transaction.commit().await?;
 
+        // Once committed, we can notify our listeners of the new received
+        // txs
+        for txid in notify_txids {
+            notify_tx(&txid, &self.notify_tx_url).await?;
+        }
+
         Ok(())
     }
 
@@ -614,15 +621,15 @@ impl Db {
         height: u32,
         txid: &[u8],
         db_tx: &mut SqliteConnection,
-    ) -> Result<u32> {
+    ) -> Result<(u32, bool)> {
         // let txid = &received_note.txid;
-        let id_tx = match sqlx::query("SELECT id_tx FROM transactions WHERE txid = ?1")
+        let result = match sqlx::query("SELECT id_tx FROM transactions WHERE txid = ?1")
             .bind(txid)
             .map(|r: SqliteRow| r.get::<u32, _>(0))
             .fetch_optional(&mut *db_tx)
             .await?
         {
-            Some(id_tx) => id_tx,
+            Some(id_tx) => (id_tx, false),
             None => {
                 let r =
                     sqlx::query("INSERT INTO transactions(txid, height, value) VALUES (?1, ?2, 0)")
@@ -632,13 +639,11 @@ impl Db {
                         .await?;
                 let id_tx = r.last_insert_rowid();
 
-                notify_tx(txid, &self.notify_tx_url).await?;
-
-                id_tx as u32
+                (id_tx as u32, true)
             }
         };
 
-        Ok(id_tx)
+        Ok(result)
     }
 
     pub fn ufvk(&self) -> &UnifiedFullViewingKey {
